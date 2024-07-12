b71476ee10e3beea2338cb6b41e3eb5401b22e68
Jesús Clemente Pérez <jmclementeperez@gmail.com>
2024-07-12 10:59:37 +0200

    Actualización

--- src/main/java/com/eoi/Fruggy/FruggyApplication.java
+++ src/main/java/com/eoi/Fruggy/FruggyApplication.java
@@ -1,6 +1,5 @@
 package com.eoi.Fruggy;
 
-import com.eoi.Fruggy.entidades.Rol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
