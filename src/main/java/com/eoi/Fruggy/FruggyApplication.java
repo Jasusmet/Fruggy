0b0a028f20904e320f1443244bbc26e0a0501548
Jesús Clemente Pérez <jmclementeperez@gmail.com>
2024-07-15 09:00:35 +0200

    Actualización 12/07/2024

--- src/main/java/com/eoi/Fruggy/FruggyApplication.java
+++ src/main/java/com/eoi/Fruggy/FruggyApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FruggyApplication {
-
-
 	public static void main(String[] args) {
 //		Usuario usuario = new Usuario();
 //		usuario.getDonaciones();
