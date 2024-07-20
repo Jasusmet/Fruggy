495c9e6282719cb7dc2f3f9e4076565422932b7f
Mihai Livadaru <lauresp96@gmail.com>
2024-07-20 02:07:07 +0200

    Actualización 20/07/2024. Parte 3. Se ha borrado entidad Direccion.java ya que solo tenía relación con Detalle, por lo tanto se puede poner todos los campos en detalle y unirlos a un Usuario. Finalmente, todos los datos de Detalle se actualizan correctamente.

--- Fruggy-developmentMarta/src/main/java/com/eoi/Fruggy/repositorios/RepoDireccion.java
+++ Fruggy-developmentMarta/src/main/java/com/eoi/Fruggy/repositorios/RepoDireccion.java
@@ -1,6 +1,5 @@
 package com.eoi.Fruggy.repositorios;
 
-import com.eoi.Fruggy.entidades.Direccion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/eoi/Fruggy/entidades/Detalle.java
+++ src/main/java/com/eoi/Fruggy/entidades/Detalle.java
@@ -21,24 +21,34 @@ import java.util.Set;
 public class Detalle implements Serializable {
 
     @Id
-    @Column(name ="detalles_id")
+    @Column(name = "detalles_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    @Column (name ="nombreUsuario",length = 50)
+    @Column(name = "nombreUsuario", length = 50)
     private String nombreUsuario;
 
-    @Column (name ="nombre",length = 255)
+    @Column(name = "nombre", length = 255)
     private String nombre;
 
-    @Column (name ="apellido",length = 255)
+    @Column(name = "apellido", length = 255)
     private String apellido;
 
-    @Column (name ="pathImagen",length = 500)
+    @Column(name = "pathImagen", length = 500)
     private String pathImagen;
 
-    @Column (name ="edad")
+    @Column(name = "edad")
     private Integer edad;
+
+    // Campos de dirección
+    @Column(name = "calle", length = 250)
+    private String calle;
+    @Column(name = "municipio", length = 250)
+    private String municipio;
+    @Column(name = "pais", length = 250)
+    private String pais;
+    @Column(name = "codigoPostal")
+    private Integer codigopostal;
 
     @OneToOne(mappedBy = "detalle", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Usuario usuario;
