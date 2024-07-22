f1818e72779a3a850b85f00837f00dc7d1f7d081
Alejandro Teixeira Muñoz <mail.alejandro.teixeira@gmail.com>
2024-07-22 19:54:08 +0200

    Select dinámico con REST API de subcategorias (en javascript)

--- src/test/java/com/eoi/Fruggy/EntidadesTest/DireccionTest.java
+++ src/test/java/com/eoi/Fruggy/EntidadesTest/DireccionTest.java
@@ -12,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBootTest
 public class DireccionTest {
 
+
     @Autowired
     private RepoDireccion repoDireccion;
     @Autowired
