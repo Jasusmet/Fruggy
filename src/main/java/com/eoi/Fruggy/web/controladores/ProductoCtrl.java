946ffa911b78896b4a746c6c6515df9c6ef33484
Jesús Clemente Pérez <jmclementeperez@gmail.com>
2024-07-30 12:19:35 +0200

    Actualización 30/07/2024

--- src/main/java/com/eoi/Fruggy/web/controladores/ProductoCtrl.java
+++ src/main/java/com/eoi/Fruggy/web/controladores/ProductoCtrl.java
@@ -1,20 +1,12 @@
 package com.eoi.Fruggy.web.controladores;
 
-import com.eoi.Fruggy.DTO.SubcategoriaDTO;
 import com.eoi.Fruggy.entidades.*;
 import com.eoi.Fruggy.servicios.*;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.data.domain.Page;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/productos")
