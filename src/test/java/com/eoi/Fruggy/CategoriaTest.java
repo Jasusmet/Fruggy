cad10937feb8ca1b4e03f26246df94b8a3b5f862
mhurgal <166488951+mhurgal@users.noreply.github.com>
2024-08-21 11:38:06 +0200

    Test comprobados: Categoria y Cesta

--- src/test/java/com/eoi/Fruggy/CategoriaTest.java
+++ src/test/java/com/eoi/Fruggy/CategoriaTest.java
@@ -42,7 +42,50 @@ class CategoriaTest {
 
 
     }
+    @Test
+    public void modificarCategoria() {
+        // buscamos la categoria a modificar
+        Optional<Categoria> cat2 = repoCategoria.findByTipoes("Español");
 
+        // cambiamos el nombre Español a Fantasma
+        Categoria categoria = cat2.get();
+        categoria.setTipo_es("Fantasma");
+
+        // grabamos la categoria en la tabla
+        repoCategoria.save(categoria);
+
+        // volvemos a cargar la tabla para comprobar si se ha modificado
+        Optional<Categoria> catModificada = repoCategoria.findByTipoes("Fantasma");
+
+        // comprobamos que el nombre ha cambiado a fantasma
+        assertEquals("Fantasma", catModificada.get().getTipo_es());
+
+    }
+
+    @Test
+    public void eliminarCategoria() {
+        // buscamos la categoria a eliminar
+        Optional<Categoria> catModificada = repoCategoria.findByTipoes("Fantasma");
+
+        // verificamos si la categoria está presente antes de eliminarla
+        if (catModificada.isPresent()) {
+            // eliminamos la categoria
+            repoCategoria.delete(catModificada.get());
+        } else {
+            System.out.println("La categoria no fue encontrada y no se puede eliminar.");
+        }
+
+        // buscamos de nuevo la categoria para confirmar si ha sido eliminada
+        Optional<Categoria> catEliminada = repoCategoria.findByTipoes("Fantasma");
+
+        // verificamos si la categoria ya no existe
+        if (catEliminada.isEmpty()) {
+            System.out.println("La categoria fue eliminada exitosamente.");
+        } else {
+            System.out.println("La categoria aún existe.");
+        }
+
+    }
 
 }
 
