82105bea9bdf54ee4c2b7c1d5e479fc868aeb098
Mihai Livadaru <lauresp96@gmail.com>
2024-08-25 01:45:52 +0200

    Modificado inicio. Carrousel de productos e intento de geolocalizacion.

--- src/main/java/com/eoi/Fruggy/web/controladores/InicioCtrl.java
+++ src/main/java/com/eoi/Fruggy/web/controladores/InicioCtrl.java
@@ -1,13 +1,34 @@
 package com.eoi.Fruggy.web.controladores;
 
+import com.eoi.Fruggy.entidades.Producto;
+import com.eoi.Fruggy.servicios.SrvcProducto;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class InicioCtrl {
+    private final SrvcProducto productosSrvc;
 
-    @GetMapping("/")
-    public String Inicio() {
+    public InicioCtrl(SrvcProducto productosSrvc) {
+        this.productosSrvc = productosSrvc;
+    }
+
+
+    @GetMapping
+    public String mostrarInicio(Model model) {
+        // Obtener los productos nuevos
+        Page<Producto> productosNuevos = productosSrvc.obtenerProductosPorNovedades(0, 10);
+        // Obtener productos con el precio más bajo
+        List<Producto> productosPrecioBajo = productosSrvc.obtenerProductosPorPrecioAscendente(0, 10).getContent();
+        model.addAttribute("productosNuevos", productosNuevos.getContent());
+        model.addAttribute("productosPrecioBajo", productosPrecioBajo);
+
         return "inicio";
     }
 }
